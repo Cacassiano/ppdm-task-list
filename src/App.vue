37bf3a595a8548e5a4057282a32b2bf3e3536152
alohamat <coelho.s@gsuite.iff.edu.br>
2026-07-16 16:12:42 -0300

    67

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+import TaskList from './components/TaskList.vue';
+const taskList = ref(["Tarefas importantes"])
+const title = ref("");
+const handleSubmit = (e) => {
+    e.preventDefault()
+    taskList.value.push(title.value);
+    title.value = "";
+  }
+</script>
+
+
+<template>
+  <form @submit="handleSubmit">
+    <input v-model="title" placeholder="crie uma nova lista de Tarefas" required/>
+    <button>Create New list</button>
+  </form>
+  <br>  
+  <ul v-for="title in taskList">
+    <TaskList :title="title" />
+  </ul>
+</template>
