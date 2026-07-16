294465464c9b0517a68878e392a876c60bbd4449
alohamat <coelho.s@gsuite.iff.edu.br>
2026-07-16 16:28:46 -0300

    Vou me matar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import TaskList from './components/TaskList.vue';
-const taskList = ref(["Tarefas importantes"])
+import { ref } from "vue";
+import TaskList from "./components/TaskList.vue";
+const taskList = ref(["Tarefas importantes"]);
 const title = ref("");
-const handleSubmit = (e) => {
-    e.preventDefault()
-    taskList.value.push(title.value);
-    title.value = "";
-  }
+const handleSubmit = (e: any) => {
+  e.preventDefault();
+  taskList.value.push(title.value);
+  title.value = "";
+};
 </script>
-
 
 <template>
   <form @submit="handleSubmit">
-    <input v-model="title" placeholder="crie uma nova lista de Tarefas" required/>
+    <input
+      v-model="title"
+      placeholder="crie uma nova lista de Tarefas"
+      required
+    />
     <button>Create New list</button>
   </form>
-  <br>  
+  <br />
   <ul v-for="title in taskList">
     <TaskList :title="title" />
   </ul>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,45 +1,43 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed } from "vue";
 const props = defineProps({
-    title: String
+  title: String,
 });
-let id = 0
+let id = 0;
 
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([])
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const todos = ref([]);
 
 const filteredTodos = computed(() => {
-  return hideCompleted.value
-    ? todos.value.filter((t) => !t.done)
-    : todos.value
-})
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
+});
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
+  todos.value.push({ id: id++, text: newTodo.value, done: false });
+  newTodo.value = "";
 }
 
 function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
+  todos.value = todos.value.filter((t) => t !== todo);
 }
 </script>
 
 <template>
-  <h1>{{title}}</h1>
+  <h1>{{ title }}</h1>
   <form @submit.prevent="addTodo">
-    <input v-model="newTodo" required placeholder="new todo">
+    <input v-model="newTodo" required placeholder="new todo" />
     <button>Add Todo</button>
   </form>
   <ul>
     <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
+      <input type="checkbox" v-model="todo.done" />
       <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
   <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+    {{ hideCompleted ? "Show all" : "Hide completed" }}
   </button>
 </template>
 
